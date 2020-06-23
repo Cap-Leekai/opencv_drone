a5be42387c0661bbc89ce7b751a1b6abb3dda8f0
KSurov <kir.surov@gmail.com>
2020-06-23 12:20:37 +0300

    added code in cv_cam

--- src/cv_camera_node.py
+++ src/cv_camera_node.py
@@ -3,19 +3,54 @@
 
 import cv2 as cv
 
-    #делаем захват картинки с камеры и пихаем её в переменную cap
-cap = cv.VideoCapture(0)
+# задаем пороги цвета
+minb =
+ming =
+minr =
+
+maxb =
+maxg =
+maxr =
+
+    # делаем захват видео с камеры в переменную cap
+cap = cv.VideoCapture("/dev/video2")  # stereo elp >> /dev/video2, /dev/video4
 
 while(True):
 
     #читаем флаг подключения камеры и картинку с камеры
     ret, frame = cap.read()
     # print(frame)
+    if ret:
 
-    cv.imshow('Camera_image', frame)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+        # переводим картинку с камеры из формата BGR в HSV
+        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+        #cv.imshow('frame', hsv)  # выводим картинку с камеры в формате HSV на экран
 
+        # делаем размытие картинки HSV
+        hsv = cv.blur(hsv, (4, 4))
+        cv.imshow('Blur', hsv)
+
+        # делаем бинаризацию картинки и пихаем её в переменную mask
+        mask = cv.inRange(hsv, (minb, ming, minr), (maxb, maxg, maxr))
+        #cv.imshow('mask', mask)
+
+        maskEr = cv.erode(mask, None, iterations=2)
+        #cv.imshow("Erode", maskEr)
+
+        maskDi = cv.dilate(maskEr, None, iterations=2)
+        cv.imshow('Dilate', maskDi)
+
+        # накладываем полученную маску на картинку с камеры переведённую в формат HSV
+        result = cv.bitwise_and(frame, frame, mask=mask)
+        cv.imshow('result', result)
+
+        # print(result)
+        if cv.waitKey(1) == 27:  # проверяем была ли нажата кнопка esc
+            break
+
+        else:
+            print("Camera not found!")
+            break
 
 
 cap.release()
